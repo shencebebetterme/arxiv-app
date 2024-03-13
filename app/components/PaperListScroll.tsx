ba54156bf4da3ad44daf41426d3b3ca4e99f800d
Ce Shen <scbebetterme@gmail.com>
2024-03-13 12:46:26 +0800

    update

--- app/components/PaperListScroll.tsx
+++ app/components/PaperListScroll.tsx
@@ -1,17 +1,3 @@
-// # Copyright (c) 2023 Baidu, Inc. All Rights Reserved.
-// #
-// # Licensed under the Apache License, Version 2.0 (the "License");
-// # you may not use this file except in compliance with the License.
-// # You may obtain a copy of the License at
-// #
-// #     http://www.apache.org/licenses/LICENSE-2.0
-// #
-// # Unless required by applicable law or agreed to in writing, software
-// # distributed under the License is distributed on an "AS IS" BASIS,
-// # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// # See the License for the specific language governing permissions and
-// # limitations under the License.
-
 "use client";
 
 import Paper, { PaperProps } from "./Paper";
@@ -51,8 +37,15 @@ function PaperList({ papers }: { papers: PaperProps[] }) {
     [papers]
   );
 
-  const { data, error, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } = useInfiniteQuery(
-    {queryKey:[queryKey]},
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    isLoading,
+  } = useInfiniteQuery(
+    { queryKey: [queryKey] },
     async ({ pageParam = 1 }) => {
       const response = await fetchData(pageParam);
       return response;
@@ -81,14 +74,13 @@ function PaperList({ papers }: { papers: PaperProps[] }) {
 
   const _papers = data?.pages.flatMap((page) => page);
 
-
   return (
     <div>
       {papers?.map((paper, i) => {
         if (papers.length - 1 === i)
           return (
             // <div ref={ref}>
-              <Paper key={paper.id} {...paper} />
+            <Paper key={paper.id} {...paper} />
             // </div>
           );
         return <Paper key={paper.id} {...paper} />;
